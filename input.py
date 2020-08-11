31460c55e38c66978bc98d1af0568ce5b1b98f83
Wonil Park <wonil.park3@outlook.com>
2020-08-11 16:16:24 -0400

    pylint practice

--- input.py
+++ input.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-name = input("What is your name? ").capitalize()
-day_of_the_week = input("What day is today? ").title()
-print(f"Hello, {name}! Happy {day_of_the_week}!") 
+
+"""
+Author: Wonil
+
+This app takes user inputs and display them
+"""
+
+def main():
+    """main function"""
+    name = input("What is your name? ").capitalize()
+    day_of_the_week = input("What day is today? ").title()
+    print(f"Hello, {name}! Happy {day_of_the_week}!")
+
+main()
